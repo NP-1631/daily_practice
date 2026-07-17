dd36c200b3cd9c47d316cc722f10cbd57165a192
NP-1631 <nandhini310522@gmail.com>
2026-07-17 14:26:41 +0530

    Time: 2 ms (98.30%) | Memory: 43 MB (93.51%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,60 +1,34 @@
+import java.util.Stack;
+
 class Solution {
     public boolean isValid(String s) {
-        Stack <Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='[')
-            {
-                stack.push(s.charAt(i));
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()) {
+
+            if(ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
             }
-            else if(s.charAt(i)==')')
-            {
-                if(stack.isEmpty())
-                {
+            else {
+
+                if(stack.isEmpty()) {
                     return false;
                 }
-                if(stack.peek()=='(')
-                   {
-                     stack.pop();
-                   }
-                   else{
-                return false;
-            }
-            }
-             else if(s.charAt(i)==']')
-            {
-                
-                if(stack.isEmpty())
-                {
+
+                char top = stack.pop();
+
+                if((ch == ')' && top == '(') ||
+                   (ch == '}' && top == '{') ||
+                   (ch == ']' && top == '[')) {
+                    
+                }
+                else{
                     return false;
                 }
-                if(stack.peek()=='[')
-                   {
-                     stack.pop();
-                   }
-                   else{
-                return false;
-            }
-            }
-             else if(s.charAt(i)=='}')
-            {
-                
-                if(stack.isEmpty())
-                {
-                    return false;
-                }
-                if(stack.peek()=='{')
-                   {
-                     stack.pop();
-                   }
-                   else{
-                return false;
-            }
-            }
-            else{
-                return false;
             }
         }
-       return stack.isEmpty();
+
+        return stack.isEmpty();
     }
 }
